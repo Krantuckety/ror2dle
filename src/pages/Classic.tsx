5828af3f54a930f99c1942937f3084d3f40cd418
Krantuckety <nferenczhalmy@gmail.com>
2025-12-17 18:17:26 -0600

    Added Classic.tsx page
    - Name

---
+++ src/pages/Classic.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Ror2Button from "../components/Ror2Button";
+
+export default function Classic()
+{
+    return (
+        <main className="min-h-screen flex flex-col items-center text-slate-100 font-bombardier">
+            
+        </main>
+    );
+}
